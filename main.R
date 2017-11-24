cfb4e8d3353c5a921f14f9dfcc4a9476f2cbcc62
supercool276 <rishabhthukral276@gmail.com>
2017-11-24 13:35:23 +0530

    Encypriotn and MAPE computation

--- main.R
+++ main.R
@@ -29,7 +29,7 @@ intensityMatrix <- readPNG(tmpName)
 intensityMatrix <- round(255*intensityMatrix)
 image(intensityMatrix ,col = gray(0:255 / 256))
 
-#Extract LSB
+#Extract original LSB
 zeros = vector(mode = "numeric",length = ncol(intensityMatrix))
 b1 <- zeros+1
 bit1 <- NULL#LSB original
@@ -37,10 +37,28 @@ for( i in (1:nrow(x))){
   vec <- bitwAnd(x[i,],b1)#lsb
   bit1 <- rbind(bit1,vec)
 }
+originalLSB <- bit1
 
 #Get Message Input
 maxLen <- (height*width)/8
 plainText <- dlgInput(message = paste0("Enter Message to encypt. Max character limit = ",maxLen), default = "My Message", gui = .GUI)$res
 
 #Encrypt using Stegnography
+intensityMatrix <- encrypt_stegnography(intensities = intensityMatrix,PT = plainText)
+
+#Extract new LSB
+bit1 <- NULL#LSB new
+for( i in (1:nrow(x))){
+  vec <- bitwAnd(intensityMatrix[i,],b1)#lsb
+  bit1 <- rbind(bit1,vec)
+}
+newLSB <- bit1
+
+#compute loss of information
+error <- newLSB - originalLSB
+changePixels <- numeric(nnzero(error))
+totalPixels <- nrow(error)*ncol(error)
+MAPE <- (changePixels*100)/(totalPixels*8)
+winDialog(type = "ok",message = paste0("Encyption Successful with ",MAPE,"% Information Loss."))
+
 
